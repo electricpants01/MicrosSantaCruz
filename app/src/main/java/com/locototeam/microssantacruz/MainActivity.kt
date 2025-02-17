393c6a68db7c445a7f6d027398991b390216db16
Christian Torrico <christian.torrico-contractor@procore.com>
2025-02-16 20:10:40 -0400

    added drawer in home screen

--- app/src/main/java/com/locototeam/microssantacruz/MainActivity.kt
+++ app/src/main/java/com/locototeam/microssantacruz/MainActivity.kt
@@ -1,17 +1,26 @@
 package com.locototeam.microssantacruz
 
+import HomeScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.locototeam.microssantacruz.ui.theme.MicrosSantaCruzTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +28,35 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MicrosSantaCruzTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Text(
-                        modifier = Modifier.padding(innerPadding),
-                        text = "hola chris"
-                    )
+                val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+                val scope = rememberCoroutineScope()
+                ModalNavigationDrawer(
+                    drawerState = drawerState,
+                    drawerContent = {
+                        ModalDrawerSheet { /* Drawer content */ }
+                    },
+                ) {
+                    Scaffold(
+                        floatingActionButton = {
+                            ExtendedFloatingActionButton(
+                                text = { Text("Show drawer") },
+                                icon = { Icon(Icons.Filled.Add, contentDescription = "") },
+                                onClick = {
+                                    scope.launch {
+                                        drawerState.apply {
+                                            if (isClosed) open() else close()
+                                        }
+                                    }
+                                }
+                            )
+                        }
+                    ) { contentPadding ->
+                        HomeScreen(
+                            modifier = Modifier
+                                .padding(contentPadding)
+                                .fillMaxSize()
+                        )
+                    }
                 }
             }
         }
