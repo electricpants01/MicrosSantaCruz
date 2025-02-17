2df0342458372f82f440746b5a598807bfe4af05
Christian Torrico <christian.torrico-contractor@procore.com>
2025-02-16 22:38:12 -0400

    add TopApp bar in MainActivity.kt

--- app/src/main/java/com/locototeam/microssantacruz/MainActivity.kt
+++ app/src/main/java/com/locototeam/microssantacruz/MainActivity.kt
@@ -5,17 +5,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
@@ -23,6 +26,7 @@ import com.locototeam.microssantacruz.ui.theme.MicrosSantaCruzTheme
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -37,6 +41,27 @@ class MainActivity : ComponentActivity() {
                     },
                 ) {
                     Scaffold(
+                        topBar = {
+                            TopAppBar(
+                                title = { Text("My App") },
+                                navigationIcon = {
+                                    IconButton(onClick = {
+                                        scope.launch {
+                                            if (drawerState.isClosed) {
+                                                drawerState.open()
+                                            } else {
+                                                drawerState.close()
+                                            }
+                                        }
+                                    }) {
+                                        Icon(
+                                            imageVector = Icons.Default.Menu,
+                                            contentDescription = "Open Drawer"
+                                        )
+                                    }
+                                }
+                            )
+                        },
                         floatingActionButton = {
                             ExtendedFloatingActionButton(
                                 text = { Text("Show drawer") },
@@ -54,7 +79,6 @@ class MainActivity : ComponentActivity() {
                         HomeScreen(
                             modifier = Modifier
                                 .padding(contentPadding)
-                                .fillMaxSize()
                         )
                     }
                 }
